069af44b7526a13e66850b602cbdef0164a0326d
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-08 09:56:29 +0530

    index file updated

--- api/index.js
+++ api/index.js
@@ -1,2 +1,24 @@
-const app = require("../backend/server");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("../backend/routes/authentication-route");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api", (req, res) => {
+  res.send("GYAN backend is running");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Mongo error:", err));
+
 module.exports = app;
