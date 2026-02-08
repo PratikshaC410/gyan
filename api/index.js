c032ee36238fb55540e7bd33ad7574221ab78f88
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-08 10:49:39 +0530

    fix in index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,3 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
-const authRoutes = require("../backend/routes/authentication-route");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-
-app.get("/api", (req, res) => {
-  res.send("GYAN backend is running");
-});
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("Mongo error:", err));
+const app = require("../backend/server");
 
 module.exports = app;
