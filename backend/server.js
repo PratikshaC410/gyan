5fac53abab03558142204ee83e2e02498664795f
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-08 14:14:50 +0530

    new chnage for api

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("GYAN backend is running");
