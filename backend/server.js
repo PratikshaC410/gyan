e9f02a5eaac8f208865046a74e5d2f00695af7f4
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-28 22:07:57 +0530

    password changed

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,7 @@ app.get("/", (req, res) => {
 
 // Connect MongoDB
 if (!mongoose.connections[0].readyState) {
-  mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(process.env.MONGO_URI);
 }
 
 module.exports = app;
